5635882e8a8e07653635ee649e79c2de6bffb2df
maxstrelok61 <71851535+maxstrelok61@users.noreply.github.com>
2020-11-05 20:18:59 +0300

    Delete MyArraySizeException.java

--- MyArraySizeException.java
+++
@@ -1,9 +0,0 @@
-package project1;
-
-import java.io.PrintStream;
-
-public class MyArraySizeException extends ArrayIndexOutOfBoundsException {
-    MyArraySizeException(String description){
-     super(description);
-    }
-}
